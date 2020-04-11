fd8e2d07312037cdc065b153f049337d3bed0324
root <root@localhost.localdomain>
2020-04-11 20:12:36 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e56b6e8ca3d196e210023f54388aa0d.js"
+  "/precache-manifest.4b2f0cf6b6cf1b9235d7dc134dba0afa.js"
 );
 
 self.addEventListener('message', (event) => {
